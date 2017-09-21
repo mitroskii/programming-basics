c74da69ebe8dd39df7c213e6f7a3797f2da91e13
mitroskii <mitro.onatsu@hotmail.com>
2017-09-21 15:44:53 +0300

    initial project

---
+++ conditional-statements/conditional-statements/conditional-statements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace conditional_statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla.");
+            Console.Write("Syötä luku: ");
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int evaluatedNumber;
+            int.TryParse(userInput, out evaluatedNumber);
+
+            if(evaluatedNumber < 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen");
+            }
+            else if(evaluatedNumber > 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on positiivinen");
+            }
+            else
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on nolla");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
