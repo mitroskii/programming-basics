4f8007f486933349174fa48224149115d9bfef6e
mitroskii <mitro.onatsu@hotmail.com>
2017-09-22 15:26:20 +0300

    update

---
+++ conditional-statements/conditional-statements/task 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma lajittelee luvut nousevaan järjestykseen");
+            bool isNumber;
+            Console.Write("Syötä luku X: ");
+           
+            Console.Write("Syötä luku Y: ");
+
+            Console.Write("Syötä luku Z: ");
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int evaluatedNumber;
+            isNumber = int.TryParse(userInput, out evaluatedNumber);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
