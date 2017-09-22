3b578e60ed2375d33c7c40612e7a6aa6a7d954f8
mitroskii <mitro.onatsu@hotmail.com>
2017-09-22 13:39:52 +0300

    task 2

---
+++ conditional-statements/conditional-statements/task 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define instructions
+            Console.WriteLine("Ohjelma selvittää onko syötetty luku pariton vai parillinen");
+            bool isNumber;
+            Console.Write("Syötä luku: ");
+            // define variables
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int evaluatedNumber;
+            isNumber = int.TryParse(userInput, out evaluatedNumber);
+
+            // program logic
+            if (evaluatedNumber % 2 == 0)
+            {
+                Console.WriteLine($"Syötit luvun {evaluatedNumber}, se on parillinen");
+            }
+            else
+            {
+                Console.WriteLine($"Syötit luvun {evaluatedNumber}, se on pariton");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
