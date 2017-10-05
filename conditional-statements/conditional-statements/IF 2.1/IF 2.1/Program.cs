b0768f9a0bf7f728c38051240aa1e1be00cc3fae
mitroskii <mitro.onatsu@hotmail.com>
2017-10-05 10:16:04 +0300

    .

--- conditional-statements/conditional-statements/IF 2.1/IF 2.1/Program.cs
+++ conditional-statements/conditional-statements/IF 2.1/IF 2.1/Program.cs
@@ -8,31 +8,54 @@ namespace IF_2._1
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Maatalousnäyttelyn lipunhinta");
-            
-                Console.Write("Ikä: ");
-                int ikä = int.Parse(Console.ReadLine());
-                if (ikä < 7)
-                    Console.Write("Lippu on ilmainen");
-                if (ikä >= 7 && ikä <= 15)
-                    Console.Write("Lippu maksaa 8.00€");
-                if (ikä >= 65)
-                    Console.Write("Lippu maksaa 8.00€");
+
+            Console.Write("Ikä: ");
+            int ikä = int.Parse(Console.ReadLine());
+            if (ikä < 7)
+                Console.Write("Lippu on ilmainen");
+            if (ikä >= 7 && ikä <= 15)
+                Console.Write("Lippu maksaa 8.00€");
+            if (ikä >= 65)
+                Console.Write("Lippu maksaa 8.00€");
 
             if (ikä > 15 && ikä < 65)
+                
             {
                 Console.WriteLine("Oletko varusmies?");
                 Console.Write("");
                 string userInput = Console.ReadLine();
                 if (Console.ReadLine() == "Kyllä")
+                    Console.WriteLine("Lippu maksaa 8.00€");
+                else
                 {
-                Console.Write("Lippu maksaa 8.00€");
-                }
-                else;
-                Console.WriteLine("Oletko opiskelija?");
+                    Console.WriteLine("Oletko opiskelija?");
+                    Console.Write("");
+                    if (Console.ReadLine() == "Kyllä")
+                        Console.WriteLine("Oletko Mtk jäsen?");
+                    Console.Write("");
+                    if (Console.ReadLine() == "Kyllä")
+                        Console.WriteLine("Lippu maksaa 6.40€");
+                
+                    else
+                    {
+                        Console.WriteLine("Oletko opiskelija?");
+                        Console.Write("");
+                        if (Console.ReadLine() == "Kyllä")
+                            Console.WriteLine("Lippu maksaa 8.80€");
 
+                        else
+                        {
+                            Console.WriteLine("Oletko Mtk jäsen?");
+                            Console.Write("");
+                            if (Console.ReadLine() == "Kyllä")
+                                Console.WriteLine("Lippu maksaa 13.60€");
+                        }
+                    }
+                }
             }
-                   Console.ReadKey();
+                Console.ReadKey();
             }
         }
     }
 
+   
---
+++ conditional-statements/conditional-statements/silmukka/silmukka/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace silmukka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma laskee kertoma. Anna luku, jonka kertoma lasketaan: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            //int.TryParse(userInput, out int number);
+            int i = 0;
+            int f = 1;
+            do
+            {
+                i = i + 1;
+                f = f * i;
+                //Console.WriteLine($"{i} kertoma on yhtäsuuri kuin{f}"); // 5!=120
+            } while (i < number);
+
+            Console.WriteLine($"Syötit {i}\n vastaus {f}"); // 5!=120
+            Console.ReadKey();
+        }
+    }
+}
