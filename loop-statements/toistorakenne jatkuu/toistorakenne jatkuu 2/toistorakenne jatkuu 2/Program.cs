29e905c99c450ce2f63bc35b0bf5b07a3fa90c10
mitroskii <mitro.onatsu@hotmail.com>
2017-10-13 11:31:02 +0300

    update

---
+++ loop-statements/toistorakenne jatkuu/toistorakenne jatkuu 2/toistorakenne jatkuu 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace toistorakenne_jatkuu_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma tulostaa kertotaulun luvuille 1-9: ");
+
+            for (int y = 1; y <= 9; y++)
+            {
+                for (int x = 1; x <= 9; x++)
+                {
+                    int tulos;
+                    tulos = x * y;
+                    Console.WriteLine($"{x}*{y} = {tulos}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
