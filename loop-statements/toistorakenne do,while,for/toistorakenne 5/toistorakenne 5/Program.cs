68a38c53703e0014cf90d1d108d85716351c402c
mitroskii <mitro.onatsu@hotmail.com>
2017-10-13 09:54:48 +0300

    done

---
+++ loop-statements/toistorakenne do,while,for/toistorakenne 5/toistorakenne 5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace toistorakenne_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma laskee parillisten ja parittomien summan. Anna luku, summat lasketaan: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            int i = 0;
+            int f = 1;
+            int evenSum = 0;
+            int oddSum = 0;
+            if (number < 0)
+            {
+                f = -1;
+            }
+
+            do
+            {
+                if (i % 2 == 0)
+                    evenSum += i;
+                else
+                    oddSum += i;
+                i++;
+            }
+            while (i <= number * f);
+            Console.WriteLine($"Parillisten lukujen summa = {evenSum * f} ja parittomien lukujen summa = {oddSum * f}");
+            Console.ReadKey();
+
+        }
+    }
+}
