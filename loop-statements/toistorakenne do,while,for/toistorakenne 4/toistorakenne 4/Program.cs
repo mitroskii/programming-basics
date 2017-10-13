ac7d9f86d3befe26b83419051499ac075b880f89
mitroskii <mitro.onatsu@hotmail.com>
2017-10-13 09:19:32 +0300

    update

---
+++ loop-statements/toistorakenne do,while,for/toistorakenne 4/toistorakenne 4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace toistorakenne_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma laskee summan positiivisesti ja negatiivisesti. Anna luku, summa lasketaan: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            int i = 0;
+            int f = 0;
+            if (number > 0)
+            {
+                do
+                {
+                    i = i + 1;
+                    f = f + i;
+                } while (i < number);
+            }
+            else
+            {
+                do
+                {
+                  i = i - 1;
+                  f = f + i;
+                } while (i > number);
+            }
+
+            Console.WriteLine($"Syötit {i}\n vastaus {f}");
+            Console.ReadKey();
+        }
+    }
+}
