b7db14e4be92f386334f261223a0c7726d198e4c
mitroskii <mitro.onatsu@hotmail.com>
2017-10-13 10:53:35 +0300

    .

---
+++ loop-statements/toistorakenne jatkuu/toistorakenne jatkuu/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace toistorakenne_jatkuu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma tulostaa kokonaislukujen 1-10 neliöjuuret: ");
+            int i = 1;
+            do
+            {
+                Console.WriteLine($"{i}  {Math.Round(Math.Sqrt(i),2)}");
+                i++;
+            }
+            while (i < 11);
+            Console.ReadKey();
+        }
+    }
+}
