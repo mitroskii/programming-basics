d55b4f8f0800cbafef38fe5989d86a6afca19d68
mitroskii <mitro.onatsu@hotmail.com>
2017-10-24 12:30:58 +0300

    update

---
+++ functions/funktiot ja aliohjelmat/funktiot ja aliohjelmat 3/funktiot ja aliohjelmat 3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace funktiot_ja_aliohjelmat_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int retNumber = numberFromRange(50,500);
+            Console.WriteLine($"Syöttämäsi luku {retNumber} on sallittu");
+            Console.ReadKey();
+        }
+        static int numberFromRange(int lowerBound, int upperBound)
+        {
+            bool isNumber;
+            int input = 0;
+            int ret = 0;
+            do
+            {
+                Console.Write("Syötä numero väliltä 50-500: ");
+                isNumber = int.TryParse(Console.ReadLine(), out input);
+                if (input >= lowerBound && input <= upperBound)
+                {
+                    ret = input;
+                    break;
+                }
+                else
+                {
+                    isNumber = false;
+                    Console.WriteLine("Numerosi ei ole sallittu, aloita alusta");
+                }
+            }
+            while (isNumber == false);
+
+            return ret;
+        }
+    }
+}
