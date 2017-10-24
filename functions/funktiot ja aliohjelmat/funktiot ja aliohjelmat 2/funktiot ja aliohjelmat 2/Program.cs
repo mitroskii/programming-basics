8e6c4169699e13a7c75de36f2cf68d5b1891c97e
mitroskii <mitro.onatsu@hotmail.com>
2017-10-24 09:39:17 +0300

    functions

---
+++ functions/funktiot ja aliohjelmat/funktiot ja aliohjelmat 2/funktiot ja aliohjelmat 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace funktiot_ja_aliohjelmat_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Syötä ohjelmalle 2 numeroa, ohjelma kertoo kumpi on pienempi.");
+            Console.WriteLine("Syötä ensimmäinen luku");
+            string userInput = Console.ReadLine();
+            int x = int.Parse(userInput);
+            Console.WriteLine("Syötä toinen luku");
+            string userInput2 = Console.ReadLine();
+            int y = int.Parse(userInput2);
+            int p = Calculator(x, y);
+            if (p == x)
+            {
+                Console.WriteLine($"{x} on pienempi kuin {y}");
+            }
+            else
+            {
+                Console.WriteLine($"{y} on pienempi kuin {x}");
+            }
+            Console.ReadKey();
+        }
+        static int Calculator(int x, int y)
+        {
+            if (x < y)
+            {
+                return x;
+            }
+            else
+            {
+                return y;
+            }
+        }
+    }
+}
---
+++ functions/funktiot ja aliohjelmat/funktiot ja aliohjelmat/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace funktiot_ja_aliohjelmat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Ohjelma pyytää yhden luvun ja palauttaa luvun tähtinä");
+            int x;
+            while (true)
+            {
+                Console.WriteLine("Tähtien lukumäärä");
+                x = int.Parse(Console.ReadLine());
+
+                if (x < 0)
+                {
+                    Console.WriteLine("Luku {0} ei ole sallittu luku", x);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            string stars = StarMachine(x);
+            Console.WriteLine($" Syötit luvun {x}. Sait tähtiä {stars}");
+            Console.ReadKey();
+        }
+        static string StarMachine(int numberOfStarts)
+        {
+            string res = string.Empty;
+            for (int i = 0; i < numberOfStarts; i++)
+            {
+                res += "*";
+            }
+            return res;
+        }
+    }
+}
