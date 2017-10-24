38067f8e84962c3e3efc2967725b840914b86acf
mitroskii <mitro.onatsu@hotmail.com>
2017-10-24 12:53:00 +0300

    done

---
+++ functions/funktiot ja aliohjelmat/funktiot ja aliohjelmat 4/funktiot ja aliohjelmat 4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace funktiot_ja_aliohjelmat_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine($"Suurin luku oli " + numerot());
+            Console.ReadKey();
+        }
+        static int numerot()
+        {
+            int num = 0;
+            int max = 0;
+            bool isNumber;
+            Console.WriteLine("Syötä 10 positiivista lukua");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{i + 1}. ");
+
+                if (isNumber = int.TryParse(Console.ReadLine(), out num) && num > 0)
+                {
+                    if (max < num)
+                    {
+                        max = num;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Väärä syöte, syötä positiivinen luku");
+                    i--;
+                }
+            }
+            return max;
+        }
+    }
+}
