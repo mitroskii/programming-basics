1c99eeeb7ba40c3190e6236b1305999ce9f59a97
mitroskii <mitro.onatsu@hotmail.com>
2017-11-02 13:47:02 +0200

    update

---
+++ arrays/Taulukkotehtävä/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Taulukkotehtävä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int[] iT = new int[100];
+            Random rnd = new Random();
+            double sum = 0;
+            // Lukujen arvonta - alustetaan taulukko
+            for (int i = 0; i < 100; i++)
+            {
+               iT[i] = rnd.Next(51);
+
+            }
+            // Laske summa ja keskiarvo
+            for (int i = 0; i < 100; i++)
+            {
+                sum = sum + iT[i];
+            }
+
+            Console.WriteLine($"Lukujen keskiarvo = {sum / 100} ");
+            Console.WriteLine($"Lukujen summa = {sum}");
+            Console.ReadKey();
+        }
+    }
+}
